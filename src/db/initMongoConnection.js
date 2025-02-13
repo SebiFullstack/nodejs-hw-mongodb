e05b0576ae413a7c1b81a4113e5756e09a5bdf2a
Sebastian4chik <sebi.ashtefcules@gmail.com>
2025-02-13 16:03:15 +0200

    update

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -2,20 +2,12 @@ import mongoose from 'mongoose';
 import { getEnvVar } from '../utils/getEnvVar.js';
 
 export const initMongoConnection = async () => {
-  try {
-    const user = getEnvVar('MONGODB_USER');
-    console.log('user', user)
-    const pwd = getEnvVar('MONGODB_PASSWORD');
-    const url = getEnvVar('MONGODB_URL');
-    const db = getEnvVar('MONGODB_DB');
+  const user = getEnvVar('MONGODB_USER');
+  const pwd = getEnvVar('MONGODB_PASSWORD');
+  const url = getEnvVar('MONGODB_URL');
+  const db = getEnvVar('MONGODB_DB');
 
-    await mongoose.connect(
-      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Students`
-    );
-
-    console.log('Mongo connection successfully established!');
-  } catch (e) {
-    console.log('Error while setting up mongo connection', e);
-    throw e;
-  }
+  await mongoose.connect(
+    `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Students`,
+  );
 };
